35f74fc85555750fb926ee20fc897b72b0a36c2f
andrewzhou08 <andrewzhou08@gmail.com>
2016-06-30 17:11:20 -0400

    Set up game

--- Game Jam/GameScene.swift
+++ Game Jam/GameScene.swift
@@ -8,14 +8,29 @@
 
 import SpriteKit
 
+enum GameState {
+    case Title, Level1, GameOver
+}
+
 class GameScene: SKScene {
+    
+    var characterNode: SKSpriteNode!
+    var touchNode: SKNode!
+    
     override func didMoveToView(view: SKView) {
-        
+        characterNode = childNodeWithName("//player") as! SKSpriteNode
+        touchNode = childNodeWithName("touchNode")
     }
     
     override func touchesBegan(touches: Set<UITouch>, withEvent event: UIEvent?) {
         for touch in touches {
-            
+            let location = touch.locationInNode(self)
+            let touchedNode = nodeAtPoint(location)
+            if(touchedNode.name == "touchLeft") {
+                print("Move left!")
+            } else if(touchedNode.name == "touchRight") {
+                print("Move right!")
+            }
         }
     }
    
