b1c32ba6d85203b4cd09db04376a11757a75e52b
andrewzhou08 <andrewzhou08@gmail.com>
2016-06-30 15:39:33 -0400

    Initial commit

---
+++ Game Jam/GameScene.swift
@@ -0,0 +1,25 @@
+//
+//  GameScene.swift
+//  Game Jam
+//
+//  Created by Andrew Zhou on 6/27/16.
+//  Copyright (c) 2016 Make School. All rights reserved.
+//
+
+import SpriteKit
+
+class GameScene: SKScene {
+    override func didMoveToView(view: SKView) {
+        
+    }
+    
+    override func touchesBegan(touches: Set<UITouch>, withEvent event: UIEvent?) {
+        for touch in touches {
+            
+        }
+    }
+   
+    override func update(currentTime: CFTimeInterval) {
+        
+    }
+}
