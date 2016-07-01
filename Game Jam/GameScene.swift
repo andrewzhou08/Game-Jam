5138f1bf4616a3f480c9720760a0682253671bb7
andrewzhou08 <andrewzhou08@gmail.com>
2016-07-01 01:18:21 -0400

    Add player movement, add art

--- Game Jam/GameScene.swift
+++ Game Jam/GameScene.swift
@@ -16,6 +16,8 @@ class GameScene: SKScene {
     
     var characterNode: SKSpriteNode!
     var touchNode: SKNode!
+    var isTouched = false
+    var touchLocation: CGPoint?
     
     override func didMoveToView(view: SKView) {
         characterNode = childNodeWithName("//player") as! SKSpriteNode
@@ -23,18 +25,42 @@ class GameScene: SKScene {
     }
     
     override func touchesBegan(touches: Set<UITouch>, withEvent event: UIEvent?) {
+        isTouched = true
         for touch in touches {
-            let location = touch.locationInNode(self)
-            let touchedNode = nodeAtPoint(location)
-            if(touchedNode.name == "touchLeft") {
-                print("Move left!")
-            } else if(touchedNode.name == "touchRight") {
-                print("Move right!")
-            }
+            touchLocation = touch.locationInNode(self)
+        }
+    }
+    
+    override func touchesMoved(touches: Set<UITouch>, withEvent event: UIEvent?) {
+        for touch in touches {
+            touchLocation = touch.locationInNode(self)
+        }
+    }
+    
+    override func touchesEnded(touches: Set<UITouch>, withEvent event: UIEvent?) {
+        isTouched = false
+        for touch in touches {
+            touchLocation = touch.locationInNode(self)
         }
     }
    
     override func update(currentTime: CFTimeInterval) {
+        if let loc = touchLocation {
+            if(loc.x < 284 && isTouched) {
+                characterNode.physicsBody?.applyImpulse(CGVectorMake(-1, 0))
+                print("loc: \(loc)")
+            }
+            if(loc.x >= 284 && isTouched){
+                characterNode.physicsBody?.applyImpulse(CGVectorMake(1, 0))
+                print("loc: \(loc)")
+            }
+        }
         
+        let velX = characterNode.physicsBody?.velocity.dx ?? 0
+        if(velX > 200) {
+            characterNode.physicsBody?.velocity.dx = 200
+        } else if(velX < -200) {
+                characterNode.physicsBody?.velocity.dx = -200
+        }
     }
 }
